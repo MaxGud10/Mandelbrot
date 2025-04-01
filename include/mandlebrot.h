edb9fc180aad7487442fd6a6f6fa4528bfdd2c61
Max <karatemaksim0110@gmail.com>
2025-04-01 23:17:33 +0300

    добавил mandelbrot_vectorized

--- include/mandlebrot.h
+++ include/mandlebrot.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include <SFML/Graphics.hpp>
 #include <immintrin.h>
 #include "stdlib.h"
@@ -29,7 +28,6 @@ enum Mode
     BY_SIMD    =  2
 };
 
-
 struct MandelBrot 
 {
     float scale;
@@ -41,16 +39,17 @@ struct MandelBrot
     uint32_t* pixels_array;
 };
 
-void mandelbrot_naive (struct MandelBrot* set);
+void        init_mandelbrot       (struct MandelBrot* set); 
 
-void get_mandel_brot_set (struct MandelBrot* set);
+void        mandelbrot_naive      (struct MandelBrot* set);
+void        mandelbrot_vectorized (struct MandelBrot* set); 
 
-void init_mandelbrot (struct MandelBrot* set); 
+void        run_performance_test  (struct MandelBrot* set);
 
-inline int handle_keyboard_input (struct MandelBrot* set, sf::Event &event); 
+inline void get_pixels            (struct MandelBrot* set, size_t index, size_t count);
+inline void get_fps               (sf::Clock &clock, sf::Text &text, struct MandelBrot* set);
 
-inline void get_pixels (struct MandelBrot* set, size_t index, size_t count);
+void        get_mandel_brot_set   (struct MandelBrot* set);
 
-inline void get_fps (sf::Clock &clock, sf::Text &text, struct MandelBrot* set);
+inline  int handle_keyboard_input (struct MandelBrot* set, sf::Event &event); 
 
-void run_performance_test (struct MandelBrot* set);
--- src/mandlebrot.cpp
+++ src/mandlebrot.cpp
@@ -1,6 +1,12 @@
-#include "../inc/mandlebrot.h"
+#include "../include/mandlebrot.h"
 
-
+void init_mandelbrot (struct MandelBrot* set) 
+{
+    set->scale        =  3.0f;
+    set->x_offset     = -0.1f;
+    set->y_offset     =  0.f ;
+    set->pixels_array = (uint32_t*) calloc (HEIGHT * WIDTH, sizeof(uint32_t));
+}
 
 void get_mandel_brot_set (struct MandelBrot* set)
 {
@@ -12,8 +18,8 @@ void get_mandel_brot_set (struct MandelBrot* set)
     sf::Texture texture = {};
     texture.loadFromImage (image);
 
-    sf::Sprite sprite = {};
-    sprite.setTexture (texture);
+    sf::Sprite sprite   = {};
+    sprite.setTexture     (texture);
 
     sf::Font font;
     font.loadFromFile ("fyodor-bold-oblique.ttf");
@@ -23,13 +29,13 @@ void get_mandel_brot_set (struct MandelBrot* set)
 
     sf::Clock clock = {};
 
-    while (window.isOpen())
+    while (window.isOpen ())
     {
         sf::Event event;
 
-        if (window.pollEvent(event))
+        if (window.pollEvent (event))
         {
-            if (event.type == sf::Event::Closed) window.close();
+            if (event.type == sf::Event::Closed) window.close ();
 
             handle_keyboard_input (set, event);
         }
@@ -48,6 +54,9 @@ void get_mandel_brot_set (struct MandelBrot* set)
 void mandelbrot_naive (struct MandelBrot* set)
 {
     const float real_dx = set->scale * (1 / (float)WIDTH);
+    // set->scale - масштаб (увеличение/уменьшение)
+    // 1 / WIDTH — нормировка на ширину экрана (перевод из пикселей в координаты [-1.5, 1.5]
+    // real_dx - шаг по оси x в комлексной плоскости
 
     for (size_t y = 0; y < HEIGHT; y++)
     {
@@ -58,6 +67,7 @@ void mandelbrot_naive (struct MandelBrot* set)
         {
             float x_n = x0;
             float y_n = y0;
+
             size_t count = 0;
 
             while (count++ < 256)
@@ -77,6 +87,80 @@ void mandelbrot_naive (struct MandelBrot* set)
     }
 }
 
+
+void mandelbrot_vectorized (struct MandelBrot* set)
+{
+    const float real_dx = set->scale * (1 / (float)WIDTH);
+
+    for (size_t y = 0; y < HEIGHT; y++) 
+    {
+            float x0 = (-((float) WIDTH  / 2)             * (1 / (float) WIDTH) + set->x_offset) * set->scale;
+            float y0 = (((float)y - ((float) HEIGHT / 2)) * (1 / (float) WIDTH) + set->y_offset) * set->scale;
+
+        for (size_t x = 0; x < WIDTH; x += VECTOR_SIZE, x0 += VECTOR_SIZE * real_dx) 
+        {
+            float X0[VECTOR_SIZE] = {x0,               x0 +     real_dx, 
+                                     x0 + 2 * real_dx, x0 + 3 * real_dx,
+                                     x0 + 4 * real_dx, x0 + 5 * real_dx, 
+                                     x0 + 6 * real_dx, x0 + 7 * real_dx};
+
+            float X_N[VECTOR_SIZE] = {};
+            float Y_N[VECTOR_SIZE] = {};
+
+            for (size_t i = 0; i < VECTOR_SIZE; i++)
+            {
+                X_N[i] = X0[i]; // z0 = 0 => X_N = 0 (z1 = z0^2 + c = c => x1 = x0)
+                Y_N[i] = y0;    // аналогично Y_N = 0
+            }
+               
+            int count = 0;
+            int real_count[VECTOR_SIZE] = {};
+
+            while (count++ < 256)
+            {
+                float X2[VECTOR_SIZE] = {}; 
+                float Y2[VECTOR_SIZE] = {}; 
+                float XY[VECTOR_SIZE] = {};
+
+                for (size_t i = 0; i < VECTOR_SIZE; i++)
+                { 
+                    X2[i] = X_N[i] * X_N[i];
+                    Y2[i] = Y_N[i] * Y_N[i];
+                    XY[i] = X_N[i] * Y_N[i];
+                }
+
+                // проверяем условие |z_n|^2 >= 100
+                int cmp[VECTOR_SIZE] = {};
+                for (size_t i = 0; i < VECTOR_SIZE; i++)
+                {
+                    if (X2[i] + Y2[i] <= 100.f) 
+                        cmp[i] = 1;                     // cmp[i] -> i-й бит mask
+                }
+
+                int mask = 0;
+                for (size_t i = 0; i < VECTOR_SIZE; i++) 
+                    mask |= (cmp[i] << i);
+
+                if (!mask) break;
+
+                // обновляем z_n 
+                for (size_t i = 0; i < VECTOR_SIZE; i++)
+                { 
+                    real_count[i] += cmp[i];
+
+                    // z_{n+1} = z_n^2 + c
+                    X_N[i] = X2[i] - Y2[i] + X0[i];
+                    Y_N[i] = XY[i] + XY[i] + y0;
+                }
+            }
+
+                for (size_t i = 0; i < VECTOR_SIZE; i++) 
+                    get_pixels (set, y * WIDTH + x + i, real_count[i]);
+        }
+    }
+}
+
+
 void run_performance_test (struct MandelBrot* set) 
 {
     switch (set->mode) 
@@ -87,11 +171,16 @@ void run_performance_test (struct MandelBrot* set)
             break;
         }
 
+        case BY_VECTOR:
+        {
+            RUN_TEST (mandelbrot_vectorized); 
+            break;
+        }
+
         default:        
             break;
     }
 }
-
 
 inline void get_pixels (struct MandelBrot* set,  size_t index, size_t count)
 {
@@ -127,6 +216,7 @@ inline int handle_keyboard_input (struct MandelBrot* set, sf::Event &event)
             default:                                                  break;
         }
     }
+
     return 0;
 }
 
@@ -142,6 +232,13 @@ inline void get_fps (sf::Clock &clock, sf::Text &text, struct MandelBrot* set)
             break;
         }
 
+        case BY_VECTOR:
+        {
+            clock.restart();
+            mandelbrot_vectorized(set); 
+            break;
+        }    
+
         default:                                     
             break;
     }
@@ -153,12 +250,4 @@ inline void get_fps (sf::Clock &clock, sf::Text &text, struct MandelBrot* set)
     sprintf (buffer, "FPS: %.2f", 1.f / elapsed_time.asSeconds ());
 
     text.setString (buffer);
-}
-
-void init_mandelbrot (struct MandelBrot* set) 
-{
-    set->scale        =  3.0f;
-    set->x_offset     = -0.1f;
-    set->y_offset     =  0.f ;
-    set->pixels_array = (uint32_t*) calloc (HEIGHT * WIDTH, sizeof(uint32_t));
 }
